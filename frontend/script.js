08ea2bbf9eca57981c0cb6075cb7b4b7c98992bf
Ugeshraja <ugeshraja007@gmail.com>
2026-07-03 14:22:07 +0530

    Connect frontend to Render backend

--- frontend/script.js
+++ frontend/script.js
@@ -185,9 +185,9 @@ document.addEventListener("DOMContentLoaded", () => {
         formData.append("image", selectedFile);
 
         try {
-            const response = await fetch("http://127.0.0.1:5000/predict", {
-                method: "POST",
-                body: formData
+            const response = await fetch("https://crop-disease-detector-backend-njdi.onrender.com/predict", {
+            method: "POST",
+            body: formData
             });
 
             if (!response.ok) {
